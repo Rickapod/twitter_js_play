35b532342a8e1032b44ff57a2d86058b92554fd8
RickDGarica <rick@empowersecurityacademy.com>
2016-09-14 20:30:02 -0400

    made success connection to twitter

--- pull_twitter.js
+++ pull_twitter.js
@@ -1,20 +1,28 @@
 var mongoose = require('mongoose');
-var config = require('./config.json');
+console.log('pass mongoose')
 var Twitter = require('twitter-node-client').Twitter;
+console.log('pass Twitter')
+var config = require('./config.json');
+console.log('pass config')
 
 var client = mongoose.connect(config.database.connection_url);
-var db = client[config.database.database_name]
-var conn = db[config.database.collection_name]
+console.log('pass client')
+// var db = mongoose.connect(config.database.database_name)
+// console.log('pass db')
+// var conn = db[config.database.collection_name]
 
 var error = function (err, response, body){console.log('ERROR [%s]', err);};
 var success = function(data) {console.log('Data [%s}', data);};
-var Twitter = require('twitter-js-client').Twitter;
-var TweetConfig = config.twitter;
+// var Twitter = require('twitter-js-client').Twitter;
+var twitter = new Twitter(config.twitter);
+console.log('pass twitter')
+console.log(twitter)
 
-var Tweets = twitter.getUserTimeline({screen_name: '', count: 10}, error, success);
-console.log(Tweets);
+var Tweets = twitter.getUserTimeline({screen_name: 'garciarick', count: 10}, error, success);
+
 
 for (tweet in Tweets){
+	console.log(tweet)
 	extractedData = {};
 	extractedData["type"] = "original";
 	extracted_data["processed"] = "false";
